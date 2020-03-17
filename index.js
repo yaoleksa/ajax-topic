3a558549d9832fa86aad70b0564b3aaa3a7c9cf8
yaoleksa <mryaremchyk@gmail.com>
2020-03-17 15:15:41 +0000

    Comment

--- index.js
+++ index.js
@@ -46,3 +46,21 @@ async function getNewFetch() {
     document.querySelector('.fclient-name').innerHTML = data.name;
     document.querySelector('.fclient-balance').innerHTML = data.temp;
 }
+
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+    .then(_ => document.querySelector('.login-form').reset());
+}
